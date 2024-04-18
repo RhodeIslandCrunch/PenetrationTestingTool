8ada32412dc100145ee0ea8a3c0fa64cd9f664bb
RhodeIslandCrunch <147107300+RhodeIslandCrunch@users.noreply.github.com>
2024-04-18 07:19:17 -0400

    Add files via upload

---
+++ setupFile.py
@@ -0,0 +1,7 @@
+import pip
+pip.main(['install','mechanize'])
+pip.main(['install','smtplib'])
+pip.main(['install','dotenv'])
+pip.main(['install','pathlib'])
+pip.main(['install','email'])
+pip.main(['install','python-dotenv'])
